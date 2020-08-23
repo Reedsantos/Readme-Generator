aed8b8210126393efa28c1cabeb0e6de3647082b
Reed Santos <reedsantos@icloud.com>
2020-08-23 10:57:33 -0400

    Added a promise and executed my functions

--- index.js
+++ index.js
@@ -1,51 +1,54 @@
 var inquirer = require("inquirer");
-inquirer
-.prompt([
-    {
-      type: "input",
-      name: "title",
-      message: "Title?"
-    },{
-        type: "input",
-        name: "description",
-        message: "Description?"
-    },{
-        type: "input",
-        name: "installation",
-        message: "Installation?"
-    },{
-        type: "input",
-        name: "usage",
-        message: "Usage?"
-    },{
-        type: "list",
-        name: "license",
-        message: "lisence?",
-        choices: [
-            "MIT",
-            "GPL",
-            "Apache"
-        ]
-    },{
-        type: "input",
-        name: "contributing",
-        message: "Contributions?"
-    },{
-        type: "input",
-        name: "tests",
-        message: "Tests?"
-    },{
-        type: "input",
-        name: "githubUsername",
-        message: "Github Username?"
-    },{
-        type: "input",
-        name: "email",
-        message: "Email Address?"
-    }]);
+var fs = require('fs');
 
-const createFile = 
-`# ${data.title} <img src='https://img.shields.io/badge/License-${data.license}-black' alt='github badge'>
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "Title?"
+        }, {
+            type: "input",
+            name: "description",
+            message: "Description?"
+        }, {
+            type: "input",
+            name: "installation",
+            message: "Installation?"
+        }, {
+            type: "input",
+            name: "usage",
+            message: "Usage?"
+        }, {
+            type: "list",
+            name: "license",
+            message: "lisence?",
+            choices: [
+                "MIT",
+                "GPL",
+                "Apache"
+            ]
+        }, {
+            type: "input",
+            name: "contributing",
+            message: "Contributions?"
+        }, {
+            type: "input",
+            name: "tests",
+            message: "Tests?"
+        }, {
+            type: "input",
+            name: "githubUsername",
+            message: "Github Username?"
+        }, {
+            type: "input",
+            name: "email",
+            message: "Email Address?"
+        }
+    ]).then(function (data) {
+        
+        const createFile =
+            `# ${data.title} <img src='https://img.shields.io/badge/License-${data.license}-black' alt='github badge'>
 ## Description
 ${data.description}
 ### Table of Contents
@@ -64,14 +67,15 @@ ${data.description}
 -  [Tests](#tests)
 
 -  [Questions](#questions)
-    
+            
 ## Installation
 ${data.installation}
-    
+            
 ## Usage
 ${data.usage}
 ## License
-${data.lisence}
+<img src='https://img.shields.io/badge/License-${data.license}-black' alt='github badge'>
+
 ## Contributing
 ${data.contributing}
 ## Tests
@@ -81,4 +85,14 @@ If anyone has any questions about my application please contact me below.
 
 ${data.email}
 
-[Github link](https://github.com/${data.githubUsername})`;
+[Github link](https://github.com/${data.githubUsername})
+`
+
+        fs.writeFile("./new-readme/README.md", createFile, function (err) {
+
+            if (err) { return console.log(err) };
+
+            console.log("Success!");
+
+        });
+    });
